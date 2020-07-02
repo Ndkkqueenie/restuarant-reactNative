9bd3ae87b4b25b66556ec3be4c3e4d240e3fce61
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2020-07-02 06:54:07 +0100

    gestures 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -20,9 +20,13 @@ const mapDispatchToProps = dispatch => ({
     postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment))
 });
 
+
 function RenderDish(props) {
 
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
+    
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
             return true;
@@ -34,6 +38,7 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -54,7 +59,8 @@ function RenderDish(props) {
     if (dish != null) {
         return(
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
-            {...panResponder.panHandlers}>
+                ref={this.handleViewRef}
+                {...panResponder.panHandlers}>
                     <Card
                         featuredTitle={dish.name}
                         image={{uri: baseUrl + dish.image}}>
