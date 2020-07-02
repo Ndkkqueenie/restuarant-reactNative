9f6c77a46431e99907d6801c36285886da3b0cc8
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2020-07-02 07:51:23 +0100

    persist redux store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -6,18 +6,28 @@ import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
 import { favorites } from './favorites';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+  }
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             dishes,
             comments,
             promotions,
             leaders,
-            favorites,
+            favorites
         }),
         applyMiddleware(thunk, logger)
     );
 
-    return store;
+    const persistor = persistStore(store)
+
+    return { persistor, store };
 }
