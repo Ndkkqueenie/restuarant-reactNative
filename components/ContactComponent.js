8b0498b2a75a9e2d3bfa99b8e21bd85a4dc4747c
Nduka O. Queendalin <Ndkkqueenie@users.noreply.github.com>
2020-07-06 08:52:56 +0100

    sending emil

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
 
 class Contact extends Component {
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
+    }
 
     render(){
         return(
@@ -13,6 +22,12 @@ class Contact extends Component {
                 <Text>Tel: +852 1234 5678</Text>
                 <Text>Fax: +852 8765 4321</Text>
                 <Text>Email:confusion@food.net</Text>
+                <Button
+                        title="Send Email"
+                        buttonStyle={{backgroundColor: "#512DA8"}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                        />
             </Card>
         );
     }
